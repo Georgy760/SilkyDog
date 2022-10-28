741417c0228f0d511400bffa3aef5a4640938272
Vitaly <46810709+Rex1262@users.noreply.github.com>
2022-10-28 23:43:20 +0300

    CommitSolanaWalletWithErrors

---
+++ Assets/README.md
@@ -0,0 +1,79 @@
+# SolPlay - Unity - SDK
+An example project to show how to use Phantom deeplinks to login and use Solana NFTs in Unity games and apps.
+
+Follow me on Twitter for more frequent updates: @SolPlay_jonas
+
+1) To use it in your game just include: 
+https://github.com/Woody4618/Solana.Unity-SDK.git (maybe a few commits ahead or behind the garbles SDK) 
+or 
+https://github.com/garbles-labs/Solana.Unity-SDK.git
+in your unity package manager.
+
+2) Import this unity package 
+[SolPlay_0_4_alpha.unitypackage.zip](https://github.com/Woody4618/SolanaUnityDeeplinkExample/files/9880685/SolPlay_0_4_alpha.unitypackage.zip)
+Or get the newest Unity package from the Releases tab on the right.
+and then import it in Unity Assets->importPackage.
+
+
+Here is a Video which explains the process step by step: 
+[https://www.youtube.com/channel/UC517QSv61gMaABWIJ412_Lw/videos](https://youtu.be/mS5Fx_yzcHw)
+
+
+If you want to participate, it's very welcome.
+
+
+Packages used: 
+
+Epic Toon FX:
+https://assetstore.unity.com/packages/vfx/particles/epic-toon-fx-57772
+
+Lunar console (Get the pro version here: 
+https://github.com/SpaceMadness/lunar-unity-console
+Pro Version: https://assetstore.unity.com/packages/tools/gui/lunar-mobile-console-pro-43800
+
+Garbels unity solana sdk. Check out their awesome game as well! Vr Pokemon! 
+https://github.com/garbles-dev/Solana.Unity/tree/master/src
+
+Solanart:
+https://github.com/allartprotocol/unity-solana-wallet
+
+Tweetnacl (removed):
+https://github.com/dchest/tweetnacl-js/blob/master/README.md#random-bytes-generation
+
+Gif loading:
+https://github.com/3DI70R/Unity-GifDecoder
+
+Flappy Bird Game: 
+https://github.com/diegolrs/Flappy-Bird
+
+Unity Ui Extensions:
+https://github.com/JohannesDeml/unity-ui-extensions.git
+
+So far the repository is only tested in IOS mobile, Android and WebGL.
+
+Done:
+- Login and getting Public key from phantom
+- Loading and caching NFTs
+- Nft meta data parsing + power level calculation
+- Deeplink to minting page
+- Deeplink to raydium token swap
+- Transactions
+- In game token and sol amount loading widget
+- WebGL support 
+- IOS Support 
+- Android Support
+- Smart contract interaction
+- Token swap using Orca WhirlPools
+- Minting NFTs using metaplex (Without candy machine)
+- Minting NFTs (from candy machine V2)
+
+Next up Todo: 
+
+- Animated Gifs
+- Gtlf models 
+- Minting NFTs (from candy machine V2 with white list tokens)
+- Maybe Staking? 
+- What else would you like?  
+
+
+
---
+++ Assets/SolPlay/Orca/OrcaSwapWidget.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Frictionless;
+using Solana.Unity.Rpc.Models;
+using Solana.Unity.SDK.Nft;
+using Solana.Unity.Wallet;
+using SolPlay.DeeplinksNftExample.Scripts;
+using SolPlay.DeeplinksNftExample.Scripts.OrcaWhirlPool;
+using SolPlay.MetaPlex;
+using SolPlay.Orca.OrcaWhirlPool;
+using SolPlay.Scripts;
+using SolPlay.Scripts.Services;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class OrcaSwapWidget : MonoBehaviour
+{
+    public PoolListItem PoolListItemPrefab;
+    public GameObject PoolListItemRoot;
+
+    public List<string> PoolIdWhiteList = new List<string>();
+
+    private void Start()
+    {
+        MessageRouter.AddHandler<WalletLoggedInMessage>(OnWalletLoggedInMessage);
+        if (ServiceFactory.Resolve<WalletHolderService>().IsLoggedIn)
+        {
+            InitPools(true);
+        }
+    }
+
+    private void OnWalletLoggedInMessage(WalletLoggedInMessage message)
+    {
+        InitPools(true);
+    }
+
+    /// <summary>
+    /// Getting all pools without the white list is very expensive since it uses get programm accounts.
+    /// public RPCs will have this call blocked. So only use it when you have our own rpc setup and want to get a list of
+    /// pools to use. I added a list of main net pools in the file OrcaMainNetPoolList.txt.
+    /// </summary>
+    /// <param name="whiteList"></param>
+    private async void InitPools(bool whiteList)
+    {
+        var pools = new List<Whirlpool.Accounts.Whirlpool>();
+        /*
+         // With this you can get a bunch of pools from the whirl pool list that is saved in the whirlpool service
+       var list = ServiceFactory.Resolve<OrcaWhirlpoolService>().OrcaApiPoolsData.whirlpools;
+       for (var index = 0; index < 20; index++)
+       {
+           var entry = list[index];
+           try
+           {
+               Whirlpool.Accounts.Whirlpool pool =
+                   await ServiceFactory.Resolve<OrcaWhirlpoolService>().GetPool(entry.address);
+               pools.Add(pool);
+               Debug.Log($"pool: {entry.address} {entry.tokenA.symbol} {entry.tokenB.symbol}");
+           }
+           catch (Exception)
+           {
+               // May not exist on dev net
+           }
+       }
+ 
+       initPools(pools);
+       return; */
+        Debug.Log("Start getting pools" + PoolIdWhiteList.Count);
+        if (whiteList)
+        {
+            foreach (var entry in PoolIdWhiteList)
+            {
+                try
+                {
+                    Whirlpool.Accounts.Whirlpool pool = await ServiceFactory.Resolve<OrcaWhirlpoolService>().GetPool(entry);
+                    pools.Add(pool);
+
+                    //Debug.Log("add pool" + pool.TokenMintA);
+                }
+                catch (Exception e)
+                {
+                    // May not exist on dev net
+                    Debug.Log($"Getting Pool error {e}");
+                }
+            }
+        }
+        else
+        {
+            // You can get all pools, but its very expensive call. So need a good RPC. public RPCs will permit it in general. 
+            // You can also use the ORCA API which is saved in ServiceFactory.Resolve<OrcaWhirlpoolService>().OrcaApiPoolsData.whirlpools
+            pools = await ServiceFactory.Resolve<OrcaWhirlpoolService>().GetPools();
+            if (pools == null)
+            {
+                ServiceFactory.Resolve<LoggingService>()
+                    .LogWarning("Could not load pools. Are you connected to the internet?", true);
+            }
+        }
+
+        initPools(pools);
+    }
+
+    private async void initPools(List<Whirlpool.Accounts.Whirlpool> pools)
+    {
+        var wallet = ServiceFactory.Resolve<WalletHolderService>().BaseWallet;
+
+        Debug.Log("pools" + pools.Count);
+        for (var index = 0; index < pools.Count; index++)
+        {
+            Whirlpool.Accounts.Whirlpool pool = pools[index];
+            PublicKey whirlPoolPda = OrcaPDAUtils.GetWhirlpoolPda(OrcaWhirlpoolService.WhirlpoolProgammId,
+                pool.WhirlpoolsConfig,
+                pool.TokenMintA, pool.TokenMintB, pool.TickSpacing);
+
+            if (!PoolIdWhiteList.Contains(whirlPoolPda))
+            {
+                //continue;
+            }
+
+            PoolData poolData = new PoolData();
+
+            poolData.Pool = pool;
+            poolData.PoolPda = whirlPoolPda;
+
+            var metadataPdaA = MetaPlexPDAUtils.GetMetadataPDA(pool.TokenMintA);
+            var metadataPdaB = MetaPlexPDAUtils.GetMetadataPDA(pool.TokenMintB);
+
+            /* Since this is very slow we get the data from the orca API instead 
+            var accountInfoMintA = await wallet.ActiveRpcClient.GetTokenMintInfoAsync(pool.TokenMintA);
+            var accountInfoMintB = await wallet.ActiveRpcClient.GetTokenMintInfoAsync(pool.TokenMintB);
+
+            if (accountInfoMintA == null || accountInfoMintA.Result == null || accountInfoMintB == null ||
+                accountInfoMintB.Result == null)
+            {
+                Debug.LogWarning($"Error:{accountInfoMintA.Reason} {accountInfoMintA} {accountInfoMintB}");
+                continue;
+            }
+                poolData.TokenMintInfoA = accountInfoMintA.Result.Value;
+                poolData.TokenMintInfoB = accountInfoMintB.Result.Value;
+            */
+
+            poolData.TokenA = ServiceFactory.Resolve<OrcaWhirlpoolService>().GetToken(pool.TokenMintA);
+            poolData.TokenB = ServiceFactory.Resolve<OrcaWhirlpoolService>().GetToken(pool.TokenMintB);
+
+            AccountInfo tokenAccountInfoA = await Nft.GetAccountData(metadataPdaA, wallet.ActiveRpcClient);
+            AccountInfo tokenAccountInfoB = await Nft.GetAccountData(metadataPdaB, wallet.ActiveRpcClient);
+
+            if (tokenAccountInfoA == null || tokenAccountInfoA.Data == null)
+            {
+                Debug.LogWarning($"Could not load meta data of mint A");
+                continue;
+            }
+
+            if (tokenAccountInfoB == null || tokenAccountInfoB.Data == null)
+            {
+                Debug.LogWarning($"Could not load meta data of mint B");
+                continue;
+            }
+
+            Metaplex metaPlexA = new Metaplex().ParseData(tokenAccountInfoA.Data[0], false);
+            Metaplex metaPlexB = new Metaplex().ParseData(tokenAccountInfoB.Data[0], false);
+
+            poolData.SymbolA = metaPlexA.data.symbol;
+            poolData.SymbolB = metaPlexB.data.symbol;
+
+            poolData.SpriteA = await GetTokenIconSprite(pool.TokenMintA, poolData.SymbolA);
+            poolData.SpriteB = await GetTokenIconSprite(pool.TokenMintB, poolData.SymbolB);
+
+            PoolListItem poolListItem = Instantiate(PoolListItemPrefab, PoolListItemRoot.transform);
+           // Debug.Log("set data" + poolData.PoolPda);
+
+            poolListItem.SetData(poolData, OpenSwapPopup);
+        }
+
+    }
+
+    /// <summary>
+    /// For some reason when trying to load the icons from token list I get a cross domain error, so for now
+    /// I just added some token icons on the resources folder. 
+    /// </summary>
+    private static async Task<Sprite> GetTokenIconSprite(string mint, string symbol)
+    {
+        foreach (var token in ServiceFactory.Resolve<OrcaWhirlpoolService>().OrcaApiTokenData.tokens)
+        {
+            var spriteFromResources = SolPlayFileLoader.LoadFromResources(symbol);
+            if (spriteFromResources != null)
+            {
+                return spriteFromResources;
+            }
+
+            if (token.mint == mint)
+            {
+                string tokenIconUrl = token.logoURI;
+                var texture = await SolPlayFileLoader.LoadFile<Texture2D>(tokenIconUrl);
+                Texture2D compressedTexture = Nft.Resize(texture, 75, 75);
+                var sprite = Sprite.Create(compressedTexture,
+                    new Rect(0.0f, 0.0f, compressedTexture.width, compressedTexture.height), new Vector2(0.5f, 0.5f),
+                    100.0f);
+                return sprite;
+            }
+        }
+
+
+        return null;
+        /*
+            Deprecated way of loading token icons from the Solana token-list
+         string tokenIconUrl =
+            $"https://github.com/solana-labs/token-list/blob/main/assets/mainnet/{mint}/logo.png?raw=true";
+        var texture = await SolPlayFileLoader.LoadFile<Texture2D>(tokenIconUrl);
+        Texture2D compressedTexture = Nft.Resize(texture, 75, 75);
+        var sprite = Sprite.Create(compressedTexture,
+            new Rect(0.0f, 0.0f, compressedTexture.width, compressedTexture.height), new Vector2(0.5f, 0.5f),
+            100.0f);
+        return sprite;*/
+    }
+
+    private void OpenSwapPopup(PoolListItem poolListItem)
+    {
+        var orcaSwapPopup = ServiceFactory.Resolve<OrcaSwapPopup>();
+        if (orcaSwapPopup == null)
+        {
+            ServiceFactory.Resolve<LoggingService>()
+                .Log("You need to add the OrcaSwapPopup to the scene.", true);
+            return;
+        }
+
+        orcaSwapPopup.Open(poolListItem.PoolData);
+    }
+}
