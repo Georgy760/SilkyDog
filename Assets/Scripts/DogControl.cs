7a84477b3cbaef53ab72f0a8a7c7ca88d36688cf
Dmitriy Chizhikov <dimm0909@gmail.com>
2022-10-30 01:45:54 +0300

    Some Fixes

--- Assets/Scripts/DogControl.cs
+++ Assets/Scripts/DogControl.cs
@@ -44,6 +44,7 @@ public class DogControl : MonoBehaviour
     {
         metres_text.text = "Metres: " + distance.ToString();
         this.transform.rotation = new Quaternion(0, 0, 0, 1);
+        this.transform.position = new Vector3(-4.48f, this.transform.position.y, this.transform.position.z);
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && is_grounded && coll_counts > 0) {
             is_grounded = false;
             rb.AddForce(transform.up * jump_speed * 100);
--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -7,13 +7,20 @@ public class Generator : MonoBehaviour
     [SerializeField] private List<GameObject> obstacles;
     [SerializeField] private Transform spawn_pos;
 
+    private void Start()
+    {
+        StartCoroutine(Create_Obstacle());
+    }
+
     private IEnumerator Create_Obstacle()
     {
         int n = Random.Range(0, obstacles.Count);
         GameObject obj = Instantiate(obstacles[n], spawn_pos);
 
-        float pass_time = 5/GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<DogControl>().objects_speed * 3;
+        float pass_time = 5/GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<DogControl>().objects_speed * 3 + 1;
 
         yield return new WaitForSeconds(pass_time);
+
+        StartCoroutine(Create_Obstacle());
     }
 }
